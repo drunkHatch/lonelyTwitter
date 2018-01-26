b42e08f1acf4e67c7834073dfc24dd459c6739d8
drunkHatch <32227266+drunkHatch@users.noreply.github.com>
2018-01-26 14:43:28 -0700

    Add files via upload

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -2,45 +2,51 @@ package ca.ualberta.cs.lonelytwitter;
 
 import java.util.Date;
 
-public abstract class Tweet implements Tweetable {
+/**
+ * Created by dezfuli on 1/16/18.
+ */
 
+public abstract class Tweet implements Tweetable {
     private String message;
     private Date date;
-    private Arraylist moodList;
 
-    public Tweet() {
-        this.moodList = new Arraylist moods;
-    }
+    Tweet(String message){
 
-    public Tweet(String message) {
         this.message = message;
+        date = new Date();
+//        message = message;
     }
 
-    public Tweet(String message, Date date) {
+    Tweet(String message, Date date){
         this.message = message;
         this.date = date;
     }
 
-    public Date getDate() {
+    public String getMessage(){
+        return message;
+    }
+
+    public void setMessage(String message) throws TweetTooLongException{
+        if (message.length() < 140){
+            this.message = message;
+        }
+        else{
+            throw new TweetTooLongException();
+        }
+    }
+
+    public Date getDate(){
         return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate(Date date){
         this.date = date;
-    }
-
-    public String getMessage() {
-        return this.message;
-    }
-
-    public void setMessage(String message) throws TweetTooLongException {
-        if (message.length() > 140) {
-            throw new TweetTooLongException();
-        } else {
-            this.message = message;
-        }
     }
 
     public abstract Boolean isImportant();
 
+    public String .toString(){
+        return date.toString() + "|" + message;
+    }
 }
+
